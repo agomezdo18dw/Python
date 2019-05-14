8fa276c7b5c9e50f6b0473d81c96af36f1681e48
djadrigo <djadrigo@djadrigo-pc>
2019-05-14 19:00:57 +0200

    pequeñas pruebas

--- EjerciciosPython/deCelsiusAFahrenheit.py
+++ EjerciciosPython/deCelsiusAFahrenheit.py
@@ -7,5 +7,5 @@ Created on 13 may. 2019
 print("¿Convertir de Celsius a Fahrenheit")
 print("¿Que temperatura desea cambiar?")
 celsius = float(input())
-fah = 1.8 * celsius + 32
+fah = (9.0/5.0) * celsius + 32
 print("La temperatura " + str(celsius) + "ºC es igual a " + str(fah) + "ºF")
